ca04d015868eea26fc574536847755a766c5eca5
jmrnd <jamarlancemiranda@gmail.com>
2023-11-29 22:45:26 +0800

    Corrected blocksize and numcacheblocks

--- src/cachesimulation/Controller.java
+++ src/cachesimulation/Controller.java
@@ -8,8 +8,8 @@ import javax.swing.JTextArea;
 
 public class Controller implements ActionListener {
     private GUI gui;
-    private int blockSize = 4; // 16
-    private int numCacheBlocks = 8; // 32
+    private int blockSize = 16; // 16
+    private int numCacheBlocks = 32; // 32
     private Cache cache;
     private Memory memory;
     String selectedTestCase;
